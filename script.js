c9b9784232143135f4845c63422ddc0d4206a51a
Mucyosoda <mucyosofa@gmail.com>
2021-08-31 10:40:38 +0200

    Update index file

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 "use strict";
-let menuBtn = document.querySelector(".menu");
-let navWrap = document.querySelector(".nav-bar");
+
+const menuBtn = document.querySelector(".menu");
+const navWrap = document.querySelector(".nav-bar");
 const closeIcon = document.getElementById("close-icon");
 
 menuBtn.addEventListener("click", () => {
@@ -17,41 +18,46 @@ closeIcon.addEventListener("click", () => {
 });
 
 /* Add Scroolspy */
-const portfolioLink = document.getElementById('portfolio-link');
-const aboutLink = document.getElementById('about-link');
-const contactLink = document.getElementById('contact-link');
+const portfolioLink = document.getElementById("portfolio-link");
+const aboutLink = document.getElementById("about-link");
+const contactLink = document.getElementById("contact-link");
 // Call Sections
-const header = document.getElementById('header');
-const headlineSection = document.getElementById('headline');
-const portfolioSection = document.getElementById('works');
-const aboutSection = document.getElementById('about');
+const header = document.getElementById("header");
+const headlineSection = document.getElementById("headline");
+const portfolioSection = document.getElementById("works");
+const aboutSection = document.getElementById("about");
 
-portfolioLink.addEventListener('click', () => {
-  navWrap.classList.remove('open');
-  navWrap.classList.remove('d-block');
-  menuBtn.classList.remove('d-none');
-  closeIcon.classList.add('d-none');
-  window.scrollTo(0, (header.clientHeight + headlineSection.clientHeight));
+portfolioLink.addEventListener("click", () => {
+  navWrap.classList.remove("open");
+  navWrap.classList.remove("d-block");
+  menuBtn.classList.remove("d-none");
+  closeIcon.classList.add("d-none");
+  window.scrollTo(0, header.clientHeight + headlineSection.clientHeight);
 });
 
-aboutLink.addEventListener('click', () => {
-  navWrap.classList.remove('open');
-  navWrap.classList.remove('d-block');
-  menuBtn.classList.remove('d-none');
-  closeIcon.classList.add('d-none');
-  window.scrollTo(0, (header.clientHeight
-    + headlineSection.clientHeight
-    + portfolioSection.clientHeight));
+aboutLink.addEventListener("click", () => {
+  navWrap.classList.remove("open");
+  navWrap.classList.remove("d-block");
+  menuBtn.classList.remove("d-none");
+  closeIcon.classList.add("d-none");
+  window.scrollTo(
+    0,
+    header.clientHeight +
+      headlineSection.clientHeight +
+      portfolioSection.clientHeight
+  );
 });
 
-contactLink.addEventListener('click', () => {
-  navWrap.classList.remove('open');
-  menuBtn.classList.remove('d-none');
-  closeIcon.classList.add('d-none');
-  navWrap.classList.remove('d-block');
-  window.scrollTo(0,
-    (header.clientHeight
-    + headlineSection.clientHeight
-    + portfolioSection.clientHeight
-    + aboutSection.clientHeight));
+contactLink.addEventListener("click", () => {
+  navWrap.classList.remove("open");
+  menuBtn.classList.remove("d-none");
+  closeIcon.classList.add("d-none");
+  navWrap.classList.remove("d-block");
+  window.scrollTo(
+    0,
+    header.clientHeight +
+      headlineSection.clientHeight +
+      portfolioSection.clientHeight +
+      aboutSection.clientHeight
+  );
 });
